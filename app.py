4d88ceb9d964d76b02acb248008296d4dfd52e3b
hazelmejora <hannemejora@gmail.com>
2026-03-04 01:33:37 +0800

    Form and report synchronized layout

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, send_file, url_for
+from flask import Flask, render_template, request, send_file
 from weasyprint import HTML
 from datetime import datetime
-import os
 
 app = Flask(__name__)
 
@@ -23,12 +22,18 @@ def generate():
         "charterer": request.form.get("charterer", ""),
         "chief_engineer": request.form.get("chief_engineer", ""),
         "master": request.form.get("master", ""),
-        "bunker1_supply": request.form.get("bunker1_supply"),
+        "bunker1_supply": request.form.get("bunker1_supply") or "",
         "bunker1_name": request.form.get("bunker1_name", ""),
         "bunker1_commenced_hour": request.form.get("bunker1_commenced_hour", ""),
         "bunker1_commenced_date": request.form.get("bunker1_commenced_date", ""),
         "bunker1_completed_hour": request.form.get("bunker1_completed_hour", ""),
         "bunker1_completed_date": request.form.get("bunker1_completed_date", ""),
+        "bunker2_supply": request.form.get("bunker2_supply") or "",
+        "bunker2_name": request.form.get("bunker2_name", ""),
+        "bunker2_commenced_hour": request.form.get("bunker2_commenced_hour", ""),
+        "bunker2_commenced_date": request.form.get("bunker2_commenced_date", ""),
+        "bunker2_completed_hour": request.form.get("bunker2_completed_hour", ""),
+        "bunker2_completed_date": request.form.get("bunker2_completed_date", ""),
         "issue_date": datetime.today().strftime('%d/%m/%Y')
     }
 
@@ -36,10 +41,7 @@ def generate():
 
     pdf_file = f"Summary_Report_{data['vessel']}.pdf"
 
-    HTML(
-        string=html,
-        base_url=request.host_url
-    ).write_pdf(pdf_file)
+    HTML(string=html, base_url=request.host_url).write_pdf(pdf_file)
 
     return send_file(pdf_file, as_attachment=True)
 
